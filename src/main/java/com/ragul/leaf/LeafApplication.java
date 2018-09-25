8a3c62bd01ea96142660426e808be2e7f4424dbd
Ragulan <Ragulan28@gmail.com>
2018-09-25 18:21:34 +0530

    default user created

--- src/main/java/com/ragul/leaf/LeafApplication.java
+++ src/main/java/com/ragul/leaf/LeafApplication.java
@@ -1,16 +1,21 @@
 package com.ragul.leaf;
 
+import com.ragul.leaf.exception.AppException;
 import com.ragul.leaf.model.Role;
 import com.ragul.leaf.model.RoleName;
+import com.ragul.leaf.model.User;
 import com.ragul.leaf.repository.RoleRepository;
+import com.ragul.leaf.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @SpringBootApplication
@@ -23,6 +28,11 @@ public class LeafApplication implements CommandLineRunner {
     @Autowired
     private RoleRepository roleRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+
     public static void main(String[] args) {
         SpringApplication.run(LeafApplication.class, args);
     }
@@ -30,11 +40,26 @@ public class LeafApplication implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (roleRepository.findAll().isEmpty()) {
+
             Role guest = new Role(RoleName.ROLE_GUEST);
             Role user = new Role(RoleName.ROLE_USER);
             Role admin = new Role(RoleName.ROLE_ADMIN);
             List<Role> roles = Arrays.asList(guest,user,admin);
             roleRepository.saveAll(roles);
+
+
+            User user1 = new User("Ragulan", "ragulan28",
+                    "ragulan28@gmail.com", "password");
+
+            user1.setPassword(passwordEncoder.encode(user1.getPassword()));
+
+            Role userRole = roleRepository.findByName(RoleName.ROLE_ADMIN)
+                    .orElseThrow(() -> new AppException("User Role not set."));
+
+            user1.setRoles(Collections.singleton(userRole));
+
+            User result = userRepository.save(user1);
+
         }
     }
 }
